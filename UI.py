f54c17a361743aa140a6735db72e19d6efa1b869
San-182004 <sneha182004@gmail.com>
2025-04-02 16:19:45 +0530

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-import keras
+import keras.models
 from keras.models import load_model
 from keras.applications.resnet import preprocess_input, decode_predictions
 

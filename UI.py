1a2200c83c72633f35c539fc849017f909e91ac2
San-182004 <sneha182004@gmail.com>
2025-04-03 19:24:22 +0530

    Update UI.py

--- UI.py
+++ UI.py
@@ -4,6 +4,17 @@ import numpy as np
 from tensorflow import keras
 
 # Streamlit UI
+st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background: url("{image_url}") no-repeat center center fixed;
+            background-size: cover;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 st.title("Brain Tumor and Alzheimer's Disease detection using Python")
 st.write("Upload an image, of appropriate filetype(jpg,jpeg,png).")
 
